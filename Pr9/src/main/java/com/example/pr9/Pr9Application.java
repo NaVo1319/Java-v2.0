19ef35b68f95a49ad0f7bfa200761959c2836ac7
Nazarkin V.I <vohammer2001@gmail.com>
2021-04-16 13:34:12 +0300

    add pr9

---
+++ Pr9/src/main/java/com/example/pr9/Pr9Application.java
@@ -0,0 +1,13 @@
+package com.example.pr9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pr9Application.class, args);
+    }
+
+}
